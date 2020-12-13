ce1d93bf8cf7034b3a236386d437b43d20694892
Corey Fuller <coreyfuller00@gmail.com>
2020-12-12 23:51:30 -0500

    Adding followers feature

--- ig_connections.py
+++ ig_connections.py
@@ -4,6 +4,7 @@ ig = open("Instagram/connections.json", "r")
 s = ig.read()
 data = json.loads(s)
 not_back = []
+un_acked = []
 for following in data["following"].keys():
     if following not in data["followers"].keys():
         not_back.append(following)
@@ -14,3 +15,15 @@ for user in not_back:
     print("%d - %s" % ( i, user))
     i+=1
     
+for request in data["follow_requests_sent"].keys():
+    if request not in data["following"].keys():
+        un_acked.append(request)
+        
+print("\n\n\n\n")
+print("Unacknowledged follow requests you sent: ")
+
+i = 1
+for user in un_acked:
+    print("%d - %s" % ( i, user))
+    i+=1
+        
