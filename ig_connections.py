a0052f02235b597073f2c8b9cd56190620b22c47
Corey Fuller <coreyfuller00@gmail.com>
2020-12-22 18:24:12 -0500

    making functions in ig_connections

--- ig_connections.py
+++ ig_connections.py
@@ -1,55 +1,64 @@
 import json
 
-def following_parser(json_fp):
-    s = json_fp.read()
-    data = json.loads(s)
+
+def getNotFollowedBack(connections):
     not_back = []
-    un_acked = []
-    for following in data["following"].keys():
-        if following not in data["followers"].keys():
-            not_back.append(following)
+    print(connections["following"])
+    for user in connections["following"].keys():
+        if user not in connections["followers"].keys():
+            not_back.append(user)
+    return not_back
+
+def getUnacknowledgedRequests(connections): 
+    unacked_requests = []      
+    for request in connections["follow_requests_sent"].keys():
+        if request not in connections["following"].keys():
+            unacked_requests.append(request)
+    return unacked_requests
     
-    print("\nPeople you follow that don't follow you back: ")
-    i = 1
-    for user in not_back:
-        print("%d - %s" % ( i, user))
-        i+=1
     
-    for request in data["follow_requests_sent"].keys():
-        if request not in data["following"].keys():
-            un_acked.append(request)
-        
-    print("\n\n\n\n")   
-    print("Unacknowledged follow requests you sent: ")
-
-    i = 1
-    for user in un_acked:
-        print("%d - %s" % ( i, user))
-        i+=1
-
-
-def likes_parser(json_fp):
-    s = json_fp.read()
+def getConnectionsInfo(filename):
+    fp = open(filename, "r")
+    s = fp.read()
     data = json.loads(s)
-    media_likes = data['media_likes']
-    likes_dict = {}
+    
+    not_following_back = getNotFollowedBack(data)
+    unacked_requests = getUnacknowledgedRequests(data)
+    
+    return not_following_back, unacked_requests
 
+def buildLikesDictionary(media_likes):
+    likes_dict = {}
     for user in media_likes:
         username = user[1]
         if username not in likes_dict:
             likes_dict[username] = 1
         else:
             likes_dict[username] += 1
-        
-    most_liked = max_user = max(likes_dict, key=likes_dict.get)
-    print("Most liked page at %d likes: %s" % (likes_dict[most_liked], most_liked))
+    return likes_dict
+
+def getFrequency(likes):
+    most = max(likes, key = likes.get)
+    least = min(likes, key = likes.get)
+    return least, most
+    
+def getLikesInfo(filename):
+    fp = open(filename, "r")
+    s = fp.read()
+    data = json.loads(s)
+    media_likes = data['media_likes']
+    likes = buildLikesDictionary(media_likes)
+    return likes
+    
     
 def run():
-    likes = open('instagram/likes.json', 'r')
-    followings = open("Instagram/connections.json", "r")
-    following_parser(followings)
-    likes_parser(likes)
-    
-    
+    connection_data = getConnectionsInfo("Instagram/connections.json")
+    likes = getLikesInfo("Instagram/likes.json")
+    frequency = getFrequency(likes)
+    print("Most liked page at %d likes: %s" % (likes[frequency[1]], frequency[1]))
+    print(connection_data[0])
+    print("\n\n\n\n\n")
+    print(connection_data[1])
+
 run()  
 
